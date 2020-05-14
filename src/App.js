b944463f7880fd0bcefd1ed17917a6387adabecb
donchan <scrambleegg7@gmail.com>
2020-05-15 01:10:40 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,155 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Grid from '@material-ui/core/Grid';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
 
-export default App;
+
+const styles = (theme) => ({
+    root: {
+        '& > *': {
+            margin: theme.spacing(1),
+        },
+    },
+    grid_root: {
+        flexGrow: 1,
+      },
+     
+});
+
+const getCourseOptions = () => {
+
+    return ({
+        method: 'GET',
+        headers: { 
+            Accept: 'application/json',
+            'Content-Type': 'application/json' ,
+        }
+        //body: JSON.stringify({ userId })
+        })
+};
+
+
+const handleResponse = (response ) => {
+
+    return response.text()
+    .then( (text) => {
+
+
+        const data = text && JSON.parse(text);
+
+        console.log("handleResponse response text --> ",data)
+
+        if (!response.ok) {
+            if (response.status !== 200) {
+                console.log("handleResponse", response)
+            }
+            const error = (data && (data.error || data.statusText) );
+            console.log("error from handleResponse", error)
+            return Promise.reject(error)
+        }
+        return data;
+    })
+
+};
+
+
+class App extends Component {
+
+
+
+    constructor(props) {
+        super();
+
+        this.state = {
+            data: []
+        }
+    }
+
+
+    handleFetchData = () => {
+
+
+        fetch("http://localhost:8080/course", getCourseOptions())
+        .then(handleResponse)
+        .then( (data) => {
+            console.log("getPosts (course) ", data.courses)
+              
+            //return data:  data.posts  
+            this.setState({
+                data: data.courses
+            })
+
+
+        })
+        
+        .catch( (err) => {
+            console.log("get error", err)            
+            //dispatch( { type: 'GETPOST_ERROR', err });
+        })
+
+
+    }
+
+
+    render () {
+
+        const {classes} = this.props;
+
+        return (
+            <div className={classes.root}>
+
+
+            <Grid container className={classes.root} spacing={2}>
+                <Grid item xs={12}>
+                    <Grid container justify="center" spacing={1}>
+                        <Grid item>
+                            <Paper className={classes.paper} >
+                                my App
+                            </Paper>
+                        </Grid>
+                    
+                    </Grid>
+                </Grid>
+                <Grid item xs={12}>
+                    <Grid container justify="center" spacing={1}>
+                        <Grid item>
+                            <Button variant="contained" color="primary" onClick={this.handleFetchData}>
+                                getData
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Grid>
+
+                <Grid item xs={12}>
+                    <Grid container justify="center" spacing={1}>
+                        
+                            {this.state.data ? this.state.data.map(res => (
+                                
+                                
+                                <Grid item key={res._id}>
+        
+                                {res._id}
+                            
+                                </Grid>
+                            
+                                                        )                            
+                            )
+                             : ""}
+
+                    </Grid>
+                </Grid>
+
+
+
+            </Grid>
+
+ 
+
+            </div>
+            );
+}}
+    
+
+export default withStyles(styles)(App);
